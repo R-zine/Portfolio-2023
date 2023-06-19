82271c6f93918f396866013991d98fee166961ff
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2023-06-19 18:39:31 +0300

    Added the 3rd project

--- src/stage1/utils/projects.js
+++ src/stage1/utils/projects.js
@@ -1,10 +1,3 @@
-import stackr1 from "stackr1.jpg";
-import stackr2 from "stackr2.jpg";
-import port20211 from "port20211.jpg";
-import port20212 from "port20212.jpg";
-
-import wiki from "wiki.jpg";
-
 class Project {
   constructor(description, tech, preview, source, site) {
     this.description = description;
@@ -19,40 +12,46 @@ export const projects = [
   new Project(
     "A block-stacking 3D game with simulated physics. Written in React and the React-three-fiber ecosystem.",
     ["React", "React-three-fiber", "CannonJS physics engine", "GSAP"],
-    [stackr1, stackr2],
+    ["./stackr1.jpg", "./stackr2.jpg"],
     "https://github.com/R-zine/stackr",
     "https://stckr.netlify.app/"
   ),
   new Project(
     "This is my dev portfolio from 2021. The site you are currently viewing was envisioned as a continuation of the old portfolio with multiple improvements. Albeit much simpler, I am happy how it turned out and it now serves as a fallback for devices with low processing power and/or small screens.",
     ["React", "React-three-fiber (for the Tech tab)", "Blender", "GSAP"],
-    [port20211, port20212],
+    ["./port20211.jpg", "./port20212.jpg"],
     "https://github.com/R-zine/PortfolioSite2021/",
     "https://ivanradev.netlify.app/"
   ),
   new Project(
-    "A real-time Wikipedia search page that offers search results with snippets together with voice recognition.",
-    ["Vanilla JS", "Speech recognition API"],
-    wiki
+    "A portfolio site for an architect, featuring many different types of animations and an unique styling approach. The website is fully responsive and has a custom 3D model in the contacts' page.",
+    [
+      "Vite/ React with routing",
+      "GSAP",
+      "Framer motion",
+      "React Spring",
+      "React Three Fiber (for the Contacts page)",
+    ],
+    ["./arch1.jpg", "./arch2.jpg"]
   ),
   new Project(
     "A real-time Wikipedia search page that offers search results with snippets together with voice recognition.",
     ["Vanilla JS", "Speech recognition API"],
-    wiki
+    "wiki"
   ),
   new Project(
     "A real-time Wikipedia search page that offers search results with snippets together with voice recognition.",
     ["Vanilla JS", "Speech recognition API"],
-    wiki
+    "wiki"
   ),
   new Project(
     "A real-time Wikipedia search page that offers search results with snippets together with voice recognition.",
     ["Vanilla JS", "Speech recognition API"],
-    wiki
+    "wiki"
   ),
   new Project(
     "A real-time Wikipedia search page that offers search results with snippets together with voice recognition.",
     ["Vanilla JS", "Speech recognition API"],
-    wiki
+    "wiki"
   ),
 ];
